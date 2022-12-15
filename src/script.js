19a9702fa8bffaa6aebd97e82ada2d7a4cd4d2eb
jmromeroes <jmromeroe@gmail.com>
2022-12-15 01:03:46 -0500

    version inicial de pagina

---
+++ src/script.js
@@ -0,0 +1,50 @@
+import './style.css';
+import gsap from 'gsap';
+import * as THREE from "three";
+import LocomotiveScroll from 'locomotive-scroll';
+
+const scroll = new LocomotiveScroll({
+  el: document.querySelector('[data-scroll-container]'),
+  smooth: true
+});
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
+
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+
+camera.position.z = 5;
+
+function animate() {
+  requestAnimationFrame( animate );
+  cube.rotation.x += 0.01;
+  cube.rotation.y += 0.01;
+
+  renderer.render(scene, camera);
+}
+
+animate();
+
+
+const lineMaterial = new THREE.LineBasicMaterial({ color: 0xffffff});
+
+const points = []
+points.push( new THREE.Vector3(-3, 0, 1));
+points.push( new THREE.Vector3(0, 1, 1));
+points.push( new THREE.Vector3(3, 0, 1));
+
+const lineGeometry = new THREE.BufferGeometry().setFromPoints(points);
+
+const line = new THREE.Line(lineGeometry, material);
+scene.add(line)
+
+const axesHelper = new THREE.AxesHelper();
+scene.add(axesHelper);
+renderer.render(scene, camera);

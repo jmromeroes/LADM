d3ba68f198d1f35dbba86884d010531ba47b8ddc
jmromeroes <jmromeroe@gmail.com>
2022-12-23 00:25:11 -0500

    Adding initial animations and images for first 2 sections

--- src/script.js
+++ src/script.js
@@ -1,14 +1,80 @@
 import './style.css';
 import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+
 import * as THREE from "three";
 import LocomotiveScroll from 'locomotive-scroll';
 
-const scroll = new LocomotiveScroll({
-  el: document.querySelector('[data-scroll-container]'),
+const locoScroll = new LocomotiveScroll({
+  el: document.querySelector('.smooth-scroll'),
   smooth: true
 });
 
-const scene = new THREE.Scene();
+gsap.registerPlugin(ScrollTrigger);
+
+locoScroll.on("scroll", ScrollTrigger.update);
+
+// tell ScrollTrigger to use these proxy methods for the ".smooth-scroll" element since Locomotive Scroll is hijacking things
+ScrollTrigger.scrollerProxy(".smooth-scroll", {
+  scrollTop(value) {
+    return arguments.length ? locoScroll.scrollTo(value, {duration: 0, disableLerp: true}) : locoScroll.scroll.instance.scroll.y;
+  }, // we don't have to define a scrollLeft because we're only scrolling vertically.
+  getBoundingClientRect() {
+    return {top: 0, left: 0, width: window.innerWidth, height: window.innerHeight};
+  },
+  // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
+  pinType: document.querySelector(".smooth-scroll").style.transform ? "transform" : "fixed"
+});
+
+// each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll. 
+ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+ScrollTrigger.defaults({ scroller: ".smooth-scroll" });
+
+gsap.set(".logo-img", {transformOrigin: "center"});
+
+var tl = gsap.timeline({
+  scrollTrigger: {
+    trigger: "#start",
+    start: "center center",
+    end: "bottom top",
+    scrub: 1,
+    pin: true
+  }
+})
+    .add("start")
+    .to("#silut", {
+      top: "50%",
+      width: "32%"
+    }, "start")
+    .to("#tipo", {
+      opacity: 1,
+      rotate: 360,
+      width: "38%"
+    }, "start")
+    .to("#signi", {
+      opacity: 1,
+      scale: 0.8,
+      left: "47%",
+      top: "33%"
+    }, "start")
+    .to("#start", {
+      //      backgroundImage: "linear-gradient(to right, rgb(37 25 100), rgb(50 101 122), rgb(40 155 89))"
+      background: "black"
+    }, "start")
+    .to("#tipo", {
+      rotate: 360*2
+    });
+
+
+window.addEventListener("mousemove", (e) => {
+  gsap.to("#mic", {
+    duration: 0.5,
+    x: e.clientX,
+    y: e.clientY
+  });
+});
+
+/*const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
 const renderer = new THREE.WebGLRenderer();
@@ -48,3 +114,4 @@ scene.add(line)
 const axesHelper = new THREE.AxesHelper();
 scene.add(axesHelper);
 renderer.render(scene, camera);
+*/
